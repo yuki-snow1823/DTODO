426c6313305a0372b5b5dda9cddd7528d5ea0cfd
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-03 09:12:16 +0900

    topページの見た目だけ完成

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -6,16 +6,16 @@
     <AddTodo @submit="addTodo" />
     <TodoList :todos="user.todos" /> -->
 
-    <h1 class="text-center orange">DTODO</h1>
+    <h1 id="title" class="text-center">DTODO</h1>
     <v-row>
-      <v-col cols="12" sm="12" md="4" lg="6">
+      <v-col cols="12" sm="12" md="6" lg="6">
         <h2 class="mb-3 text-center">タスク管理をゲームに！</h2>
         <h3>これは紹介文です。これは紹介文です。これは紹介文です。これは紹介文です。これは紹介文です。
           これは紹介文です。これは紹介文です。これは紹介文です。これは紹介文です。これは紹介文です。これは紹介</h3>
       </v-col>
-      <v-col cols="12" sm="12" md="4" lg="6">
+      <v-col cols="12" sm="12" md="6" lg="6">
         <form>
-          <h2>新規登録はコチラ</h2>
+          <h2 class="text-center">新規登録はコチラ</h2>
           <v-text-field v-model="name" :counter="10" label="Name" data-vv-name="name" required></v-text-field>
           <v-text-field v-model="email" :counter="20" label="Email" data-vv-name="email" required></v-text-field>
           <v-text-field v-model="password" label="password" data-vv-name="password" required
@@ -30,6 +30,50 @@
       </v-col>
     </v-row>
 
+    <v-row class="introduction">
+      <v-col cols="12" sm="12" md="4" lg="4">
+        <v-img width="50%" height="50%"
+          src="https://4.bp.blogspot.com/-PDD_C43ZQ9A/V5AXrn_GD6I/AAAAAAAA8Z4/0KzHBvARnV4itcdlfhCpbtPSTwHYqPzcgCLcB/s800/vr_game_pad.png">
+        </v-img>
+        <h3>特徴の１つめです</h3>
+        <p>説明文です。説明文です。説明文です。説明文です。説明文です。説明文です。
+          説明文です。説明文です。説明文です。説明文です。説明文です。説明文です。
+        </p>
+      </v-col>
+      <v-col cols="12" sm="12" md="4" lg="4">
+        <v-img width="50%" height="50%"
+          src="https://4.bp.blogspot.com/-PDD_C43ZQ9A/V5AXrn_GD6I/AAAAAAAA8Z4/0KzHBvARnV4itcdlfhCpbtPSTwHYqPzcgCLcB/s800/vr_game_pad.png">
+        </v-img>
+        <h3>特徴の１つめです</h3>
+        <p>説明文です。説明文です。説明文です。説明文です。説明文です。説明文です。
+          説明文です。説明文です。説明文です。説明文です。説明文です。説明文です。
+        </p>
+      </v-col>
+      <v-col cols="12" sm="12" md="4" lg="4">
+        <v-img width="50%" height="50%"
+          src="https://4.bp.blogspot.com/-PDD_C43ZQ9A/V5AXrn_GD6I/AAAAAAAA8Z4/0KzHBvARnV4itcdlfhCpbtPSTwHYqPzcgCLcB/s800/vr_game_pad.png">
+        </v-img>
+
+        <h3>特徴の１つめです</h3>
+        <p>説明文です。説明文です。説明文です。説明文です。説明文です。説明文です。
+          説明文です。説明文です。説明文です。説明文です。説明文です。説明文です。
+        </p>
+      </v-col>
+    </v-row>
+
+    <v-row>
+<v-col cols="8" sm="8" md="8" lg="8" offset="2">
+        <v-carousel>
+        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="fade-transition"
+          transition="fade-transition"></v-carousel-item>
+      </v-carousel>
+</v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" sm="12" md="6" lg="6">新規登録（押したら画面上に）</v-col>
+      <v-col cols="12" sm="12" md="6" lg="6">ログイン（押したらウィンドウ）</v-col>
+    </v-row>
 
 
   </v-container>
@@ -83,4 +127,25 @@
 </script>
 
 <style>
+  #title {
+    display: inline-block;
+    background-color: #fc7b03;
+    text-align: center;
+    margin: 0 auto;
+  }
+  .introduction {
+    margin-top: 50px;
+  }
+  .introduction h3 {
+    text-align: center;
+    margin: 0 auto;
+    color: aqua !important;
+  }
+  /* 指定がうまくいかない */
+  .v-img {
+    text-align: center;
+  }
+  .v-window__container {
+    border: solid 5px white;
+  }
 </style>
