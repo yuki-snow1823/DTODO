67cf2a389dff9c8921b7391c85eb4adc6d21bad9
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-10 08:23:21 +0900

    [add]未ログイン時のトグルメニューにトップ追加

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -17,7 +17,9 @@
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title class="pl-0">
-        <router-link to="/" class="toolbar-title"><v-icon class="mb-2" size="38">mdi-skull-outline</v-icon><span class="title-first">D</span>TODO</router-link>
+        <router-link to="/" class="toolbar-title">
+          <v-icon class="mb-2" size="38">mdi-skull-outline</v-icon><span class="title-first">D</span>TODO
+        </router-link>
       </v-toolbar-title>
       <v-spacer />
     </v-app-bar>
@@ -34,81 +36,86 @@
 </template>
 
 <script>
-import Loading from "@/components/Loading";
-import Success from "@/components/Success";
+  import Loading from "@/components/Loading";
+  import Success from "@/components/Success";
 
 
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "DTODO"
-    };
-  },
-    components: {
-    Loading,
-    Success
-  },
-  computed: {
-    user() {
-      return this.$store.state.currentUser;
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: "DTODO"
+      };
     },
-    items() {
-      if (this.user) {
-        return [
-          {
-            icon: "mdi-heart",
-            title: "TODO",
-            to: "/user"
-          },
-          {
-            icon: "mdi-lock",
-            title: "REWARD",
-            to: "/reward"
-          }
-        ];
-      } else {
-        return [
-          {
-            icon: "mdi-key-variant",
-            title: "ログイン",
-            to: "/login"
-          },
-          {
-            icon: "mdi-account-arrow-right",
-            title: "新規登録",
-            to: "/signup"
-          }
-        ];
+    components: {
+      Loading,
+      Success
+    },
+    computed: {
+      user() {
+        return this.$store.state.currentUser;
+      },
+      items() {
+        if (this.user) {
+          return [
+            {
+              icon: "mdi-heart",
+              title: "TODO",
+              to: "/user"
+            },
+            {
+              icon: "mdi-lock",
+              title: "REWARD",
+              to: "/reward"
+            }
+          ];
+        } else {
+          return [
+            {
+              icon: "mdi-skull-crossbones",
+              title: "トップ",
+              to: "/"
+            },
+            {
+              icon: "mdi-key-variant",
+              title: "ログイン",
+              to: "/login"
+            },
+            {
+              icon: "mdi-account-arrow-right",
+              title: "新規登録",
+              to: "/signup"
+            }
+          ];
+        }
       }
     }
- }
-}
-
+  }
 </script>
 
 <style lang="scss">
+  $main-color: #fc7b03;
 
-$main-color: #fc7b03;
+  .app {
+    .toolbar-title {
+      color: white;
+      text-decoration: none;
+      font-family: 'Comic Sans MS';
+      font-size: 30px;
+      font-weight: bold;
 
-.app{
-  .toolbar-title {
-    color: white;
-    text-decoration: none;
-    font-family: 'Comic Sans MS';
-    font-size: 30px;
-    font-weight: bold;
-    .title-first {
-      color: $main-color;
+      .title-first {
+        color: $main-color;
+      }
+    }
+
+    .v-content__wrap {
+      background-color: black !important;
     }
   }
-  .v-content__wrap {
-    background-color: black !important;
-  }
-}
 </style>
