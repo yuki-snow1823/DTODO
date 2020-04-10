aaea7a806667ac1b39b4ac64e5e4de399a7aacb3
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-10 23:17:16 +0900

    微修正

--- api/spec/controllers/todos_controller_spec.rb
+++ api/spec/controllers/todos_controller_spec.rb
@@ -3,10 +3,9 @@ require 'rails_helper'
 describe V1::TodosController , type: :controller do
   
     describe 'createアクションについて' do
-    it 'titleとpointが設定してある場合、実行されるか' do
+    it 'todoが期待される値を持つ' do
       todo = create(:todo)
       
-
       expect(todo).to be_valid
     end
   end
