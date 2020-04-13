42022605692a7cf6c2452bedbf1c841c368ee0f1
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-13 15:23:41 +0900

    [add]既存のバリデーションに関するTodoモデルのテスト完了

--- api/spec/models/todo_spec.rb
+++ api/spec/models/todo_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  describe 'Todoを登録できる' do
+  describe '動作確認のテスト' do
     before do
       @user = FactoryBot.create(:user)
     end
@@ -13,7 +13,7 @@ RSpec.describe Todo, type: :model do
         title: "test",
         point: 1,
       )
-      expect(user).to be_valid
+      expect(todo).to be_valid
     end
 
     it 'Todoを削除' do
@@ -22,5 +22,46 @@ RSpec.describe Todo, type: :model do
       todo.destroy
       expect{Todo.find(1)}.to raise_exception(ActiveRecord::RecordNotFound)
     end
+
+    it 'Todoのtitleを編集' do
+      user = @user
+      todo =FactoryBot.create(:todo)
+      todo.update(title: "new-title")
+      expect(todo.title).to eq "new-title"
+    end
+
+    it 'Todoのpointを編集' do
+      user = @user
+      todo =FactoryBot.create(:todo)
+      todo.update(point: 2)
+      expect(todo.point).to eq 2
+    end
+  
+  end
+
+  describe 'バリデーションのテスト' do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    it 'Todoはtitleが空の場合保存されない' do
+      user = @user
+      todo = Todo.new(
+        user_id: 1,
+        title: "",
+        point: 1,
+      )
+      todo.valid?
+      expect(todo.errors[:title]).to include("can't be blank")
+    end
+
+    # it 'Todoを削除' do
+    #   user = @user
+    #   todo =FactoryBot.create(:todo)
+    #   todo.destroy
+    #   expect{Todo.find(1)}.to raise_exception(ActiveRecord::RecordNotFound)
+    # end
+
+  
   end
 end
