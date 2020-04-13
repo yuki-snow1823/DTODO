5d80571a23cbe0cf5ceb895ea9683513b0de5a45
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-13 23:19:31 +0900

    [add]pages/indexのテスト着手/titleの表示確認まで

--- front/spec/pages/index.spec.js
+++ front/spec/pages/index.spec.js
@@ -2,6 +2,7 @@ import Index from "./../../pages/index.vue";
 import Vuex from 'vuex'
 import { mount, createLocalVue } from '@vue/test-utils'
 import * as indexStore from './../../store'
+// @だとうまく読み込めなかったため、直接相対パスを指定しています。
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -17,9 +18,14 @@ describe('pages/index.vue', () => {
     })
   })
 
-  describe('template', () => {
+  describe('表示に関するテスト', () => {
     test('タイトルが存在すること', () => {
-      expect(wrapper.contains('.index-explain')).toBe(true)
+      expect(wrapper.contains('.index-title')).toBe(true)
+    })
+
+    test('ログインしているときに、新規登録フォームが表示されない',() => {
+      expect(wrapper.contains('#index-signup')).toBe(true)
+      // 不明
     })
   })
   
