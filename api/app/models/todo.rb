0ef6772b2d6d312695e8688bda59eaa8ccc90f6b
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-15 08:34:18 +0900

    [fix]todoのバリデーションを20文字以下に変更

--- api/app/models/todo.rb
+++ api/app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ApplicationRecord
       validates :title, presence: true
-      validates :title, length: { in: 1..20 }
+      validates :title, length: { maximum: 20 }
       validates :point, presence: true
       belongs_to :user, optional: true
 end
