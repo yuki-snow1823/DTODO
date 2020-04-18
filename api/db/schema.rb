d4d3a289c0892117b8f8977f02fbdf66e46ff07c
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-19 00:59:27 +0900

    [add/api]sortカラム(マイグレーション済み)

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_231525) do
+ActiveRecord::Schema.define(version: 2020_04_18_155753) do
 
   create_table "level_settings", force: :cascade do |t|
     t.integer "level"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_231525) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "point"
+    t.integer "sort", default: 0
   end
 
   create_table "users", force: :cascade do |t|
