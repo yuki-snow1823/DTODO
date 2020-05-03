ae380e71a45dc492dc989c10b196e14a1b07b746
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-03 14:28:48 +0900

    [add]AddTodoテストの修正/

--- front/spec/components/AddTodo.spec.js
+++ front/spec/components/AddTodo.spec.js
@@ -2,24 +2,58 @@ import AddTodo from '@/components/AddTodo'
 import {
   mount
 } from '@vue/test-utils'
-
+import Vuex from 'vuex';
 
 let wrapper
+// let store
+
+// const localVue = createLocalVue();
+// localVue.use(Vuex);
 
 beforeEach(() => {
-  wrapper = mount(AddTodo)
+  wrapper = mount(AddTodo, {
+    propsData: {
+      todo: {
+        title: "",
+        point: null
+      }
+    },
+    mocks: {
+      $store: {
+        state: {
+          currentUser: {
+            user: {
+              experience_point: 0
+            },
+            todo: {
+              point: "",
+              title: "",
+            },
+            reward: {
+              point: "",
+              title: "",
+            },
+            todos: [],
+            rewards: [],
+            untilPercentage: null,
+            untilLevel: null,
+          },
+          loading: false,
+          notification: {
+            status: false,
+            message: ""
+          },
+          errors: []
+        }
+      },
+    },
+  })
 })
 
 describe('DOMイベントのテスト', () => {
 
   it('Testの詳細', () => {
-    const hundleSubmit = jest.fn()
-
-    wrapper.setMethods({
-      hundleSubmit
-    })
-
-    wrapper.find('.todo-btn').trigger('click')
-    expect(hundleSubmit).toBeCalled()
+    // wrapper.find('.todo-btn').trigger('click')
+    expect (wrapper.find('errors')).toBeTruthy()
   })
 })
