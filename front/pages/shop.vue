740ae23e9ad3cd3b18785cf6901918a0af68ad3b
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-09 10:39:48 +0900

    [start]shop機能構想(ページ追加、表示確認、基本設定） #99

---
+++ front/pages/shop.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <p>ここはショップです。</p>
+        <ul>
+          <li>すごいアイコン<v-btn>購入</v-btn></li>
+          <li>普通のアイコン</li>
+          <li>ダサいアイコン</li>
+        </ul>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      experience_point: "",
+      show1: false,
+      show2: false,
+      error: "",
+      showContent: false
+    };
+  },
+  fetch({ store, redirect }) {
+    store.watch(
+      state => state.currentUser,
+      (newUser, oldUser) => {
+        console.log(newUser, oldUser);
+        if (!newUser) {
+          return redirect("/");
+        }
+      }
+    );
+  },
+  computed: {
+    // currentUserと書くだけで表示してくれるメリットがある
+    currentUser() {
+      return this.$store.state.currentUser;
+    }
+  }
+};
+</script>
