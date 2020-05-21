fc217f0b0ef812528b1b04c3b4228c340c56e368
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-21 19:48:19 +0900

    [add]wrapperに何も値が入らない(error/user.vue)

--- front/spec/components/user.spec.js
+++ front/spec/components/user.spec.js
@@ -1,33 +1,56 @@
-import user from "@/pages/user";
-import { mount, createLocalVue } from "@vue/test-utils";
-import Vuex from "vuex";
-import * as store from "@/store";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
+import user from '@/pages/user.vue'
 
-let wrapper;
-let vuetify;
+Vue.use(Vuetify)
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
+const localVue = createLocalVue()
 
-beforeEach(() => {
-  vuetify = new Vuetify();
-  wrapper = mount(user, {
-    propsData: {
-      experience_point: "",
-      show1: false,
-      show2: false,
-      error: "",
-      showContent: false
-    },
-    store: store,
-    localVue,
-    vuetify
-  });
-});
-
-describe("AddTodoに関するテスト", () => {
+describe('CustomCard.vue', () => {
+  let vuetify
+  let wrapper
+  beforeEach(() => {
+    vuetify = new Vuetify()
+    wrapper = mount(user, {
+      localVue,
+      vuetify,
+    })
+  })
   test("TODOが追加されること", () => {
-    wrapper.find('input[type="text"]').setValue(1);
+    console.log(wrapper);
     expect(wrapper.vm.todo.point).toBe("1");
   });
-});
+})
+// import user from "@/pages/user";
+// import { mount, createLocalVue } from "@vue/test-utils";
+// import Vuetify from "vuetify";
+// import Vuex from "vuex";
+// import * as store from "@/store";
+
+// let wrapper;
+// let vuetify;
+
+// const localVue = createLocalVue();
+// localVue.use(Vuex);
+
+// beforeEach(() => {
+//   vuetify = new Vuetify();
+//   wrapper = mount(user, {
+//     computed: {
+//       currentUser() {
+//         return 1
+//       }
+//     },
+//     propsData: {
+//       experience_point: "",
+//       show1: false,
+//       show2: false,
+//       error: "",
+//       showContent: false
+//     },
+//     store: store,
+//     localVue,
+//     vuetify
+//   });
+// });
