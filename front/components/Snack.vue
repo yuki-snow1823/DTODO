73a0d70749f885b69d75635348d619a011d0528e
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-30 21:48:03 +0900

    [add]スナックメッセージのコンポーネント切り出し

---
+++ front/components/Snack.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
+    {{ snackText }}
+    <v-btn text @click="snack = false">Close</v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        snack: false,
+        snackColor: "",
+        snackText: "",
+      };
+    },
+  };
+</script>
+
+<style lang="scss">
+
+</style>
