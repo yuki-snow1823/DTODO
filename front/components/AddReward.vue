8a5b5001a59aa005187fd1204b3a0643099d2259
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-12 10:59:01 +0900

    [add]AddRewardのリファクタリング

--- front/components/AddReward.vue
+++ front/components/AddReward.vue
@@ -1,96 +1,107 @@
 <template>
-      <v-row class="add-reward">
-        <v-col id="v-step-0" class="pr-0 pb-0" cols="4" xs="2" sm="2" md="2" lg="2">
-          <v-select label="TP" v-model="reward.point" :items="items" outlined></v-select>
-        </v-col>
-        <v-col class="pl-0 pb-0" cols="8" xs="8" sm="8" md="8" lg="8">
-          <v-text-field id="reward-title" v-model="reward.title" :counter="20" label="ごほうびの内容" required outlined>
-          </v-text-field>
-        </v-col>
-        <v-col class="px-0 pb-0" cols="12" xs="2" sm="2" md="2" lg="2">
-          <v-hover v-slot:default="{ hover }">
-            <v-btn class="reward-btn" @click="handleSubmit">
-              <v-icon v-text="hover ? 'mdi-heart' : ''"></v-icon>
-              登録
-            </v-btn>
-          </v-hover>
-        </v-col>
-      </v-row>
+  <v-row class="add-reward">
+    <v-col id="v-step-0" class="pr-0 pb-0" cols="4" xs="2" sm="2" md="2" lg="2">
+      <v-select
+        label="TP"
+        v-model="reward.point"
+        :items="items"
+        outlined
+      ></v-select>
+    </v-col>
+    <v-col class="pl-0 pb-0" cols="8" xs="8" sm="8" md="8" lg="8">
+      <v-text-field
+        id="reward-title"
+        v-model="reward.title"
+        :counter="20"
+        label="ごほうびの内容"
+        required
+        outlined
+      >
+      </v-text-field>
+    </v-col>
+    <v-col class="px-0 pb-0" cols="12" xs="2" sm="2" md="2" lg="2">
+      <v-hover v-slot:default="{ hover }">
+        <v-btn class="reward-btn" @click="handleSubmit">
+          <v-icon v-text="hover ? 'mdi-heart' : ''"></v-icon>
+          登録
+        </v-btn>
+      </v-hover>
+    </v-col>
+  </v-row>
 </template>
 <script>
-  const numberRange = [...Array(9).keys()].map(i => ++i);
-  export default {
-    props: {
-      reward: {
-        type: Object,
-        default: {
-          title: "",
-          point: null
-        },
-        // プロパティを必ず渡し、エラーを防ぐために記載
-        required: true
-      }
-    },
-    data() {
-      return {
-        items: numberRange,
-      };
-    },
-    methods: {
-      handleSubmit() {
-        const reward = {
-          title: this.reward.title,
-          user_id: this.$store.state.currentUser.user.id,
-          point: this.reward.point
-        };
-        this.$emit("submit", reward);
-        // 登録後に入力した値をリセットしています
-        this.reward.title = "";
-        this.reward.point = "";
-      }
-    },
-    created() {
-      this.$vuetify.lang = {
-        t: () => {}
-      };
-      //testを実行する際に直接関係ないエラーを回避する為に記述してあります。
+const numberRange = [...Array(9).keys()].map(i => ++i);
+export default {
+  props: {
+    reward: {
+      type: Object,
+      default: {
+        title: "",
+        point: null
+      },
+      // プロパティを必ず渡し、エラーを防ぐために記載
+      required: true
     }
-  };
+  },
+  data() {
+    return {
+      items: numberRange
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const reward = {
+        title: this.reward.title,
+        user_id: this.$store.state.currentUser.user.id,
+        point: this.reward.point
+      };
+      this.$emit("submit", reward);
+      // 登録後に入力した値をリセットしています
+      this.reward.title = "";
+      this.reward.point = "";
+    }
+  },
+  created() {
+    this.$vuetify.lang = {
+      t: () => {}
+    };
+    //testを実行する際に直接関係ないエラーを回避する為に記述してあります。
+  }
+};
 </script>
 
 <style lang="scss">
-  $main-color: #fc7b03;
+$main-color: #fc7b03;
 
-  @mixin btn {
-    background-color: black !important;
-    border: 2px solid $main-color;
-    color: $main-color;
-    display: inline-block;
-    font-weight: bold;
-    font-size: 25px;
-    padding-bottom: 35px !important;
-    padding-top: 17px !important;
-    margin-left: 5%;
-    width: 90%;
-  }
+@mixin btn {
+  background-color: black !important;
+  border: 2px solid $main-color;
+  color: $main-color;
+  display: inline-block;
+  font-weight: bold;
+  font-size: 25px;
+  padding-bottom: 35px !important;
+  padding-top: 17px !important;
+  margin-left: 5%;
+  width: 90%;
+}
 
-  .add-reward {
+.add-reward {
+  .reward-btn {
+    @include btn;
 
-    .reward-btn {
-      @include btn;
-
-      &:hover {
-        border: 2px solid yellow;
-        color: yellow;
-      }
-
-      span {
-        margin-top: -9px;
-      }
+    &:hover {
+      border: 2px solid yellow;
+      color: yellow;
     }
 
-    .mdi-heart {
-      color: red !important;
+    span {
+      margin-top: -9px;
     }
   }
+
+  .mdi-heart {
+    color: red !important;
+  }
+}
 </style>
